a210eca03da809ca1610ef1ab78f38422d5c7b3d
mashmindev <mashmindev@gmail.com>
2022-12-12 18:07:01 +0800

    chore: product pagination added

--- config/app.php
+++ config/app.php
@@ -195,6 +195,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        \Nuwave\Lighthouse\WhereConditions\WhereConditionsServiceProvider::class,
+
+
     ],
 
     /*
